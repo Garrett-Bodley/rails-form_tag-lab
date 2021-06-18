2e8befeeb2ebba68d849a5558adaf85280c9579f
Garrett-Bodley <Garrett.Bodley@gmail.com>
2021-06-18 15:47:26 -0400

    Archive Flatiron code.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -8,4 +8,15 @@ class StudentsController < ApplicationController
     @student = Student.find(params[:id])
   end
 
+  def new
+    @student_name = session[:student_name]
+    session[:student_name] = nil
+  end
+
+  def create
+    @student = Student.create(first_name: params[:student][:first_name], last_name: params[:student][:last_name])
+    session[:student_name] = @student.to_s
+    redirect_to new_student_path
+  end
+
 end
